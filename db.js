7f2789c8f3a4e97d72f62370ac8f135eba3c7b8a
ss <>
2020-02-15 11:38:09 +0530

    added db

---
+++ db.js
@@ -0,0 +1,29 @@
+const MongoClient = require("mongodb").MongoClient;
+
+const uri = "mongodb+srv://sivasankar9:raknas@9@cluster0-fnz8t.mongodb.net/test?retryWrites=true&w=majority";
+
+const dbConnectionUrl = uri;
+
+function initialize(
+    dbName,
+    dbCollectionName,
+    successCallback,
+    failureCallback
+) {
+    MongoClient.connect(dbConnectionUrl, function(err, dbInstance) {
+        if (err) {
+            console.log(`[MongoDB connection] ERROR: ${err}`);
+            failureCallback(err); // this should be "caught" by the calling function
+        } else {
+            const dbObject = dbInstance.db(dbName);
+            const dbCollection = dbObject.collection(dbCollectionName);
+            console.log("[MongoDB connection] SUCCESS");
+
+            successCallback(dbCollection);
+        }
+    });
+}
+
+module.exports = {
+    initialize
+};
--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const mongoose  = require('mongodb');
+const db = require("./db");
+
+
+const PORT = process.env.PORT || 5000;
+const dbName = "test";
+const collectionName = "devices";
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -8,3 +14,19 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+
+console.log("EE::1:2:---------------");
+db.initialize(dbName,collectionName,
+  function succ(dbCollection){
+    // console.log("yses:",a.find({pin:520012}))
+    dbCollection.find().toArray(function(err, result) {
+      if (err) throw err;
+      console.log(">>",result);
+  });
+  
+  },
+  function fail(a){console.log("ooh",a)}
+  
+  );
